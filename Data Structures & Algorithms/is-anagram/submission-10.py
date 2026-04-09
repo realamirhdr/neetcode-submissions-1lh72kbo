863cb2f1deccc2296f9982fae618496f8514a0fb
Amirreza Heidari <99999604+realamirhdr@users.noreply.github.com>
2026-04-10 01:48:46 +0330

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        dicS = {}
+        dicT = {}
+
+        for i in range(len(s)):
+            dicS[s[i]] = dicS.get(s[i], 0) + 1
+            dicT[t[i]] = dicT.get(t[i], 0) + 1
+
+
+        return dicT == dicS
